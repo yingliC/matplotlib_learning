2708f06252f3056d1521cd084f62014c2ad49a0e
yingliC <zorapea@126.com>
2019-09-08 19:15:17 +0800

    move and calssify

--- project1.py
+++ project1.py
@@ -1,3 +1,19 @@
+'''
+三种方式：
+pyplot:经典高层封装，到目前为止
+pylab： 将matplotlib 和 numpy 合并的模块，模拟matlab的编程环境
+面向对象的方式：matplotlib的精髓，更基础和底层的方式
+
+
+三种方式的优劣：
+pyplot：简单易用。交互使用方便，可以根据命令实时作图，但底层能力不足。
+pylab： 完全封装，环境最接近matlab。不推荐使用
+面向对象（Object—Oriented）的方式：接近Matplotlib基础和底层的方式，难度稍大。但定制能力强。而且是Matplotlib的精髓。
+
+总结：实战中推荐，根据需求，综合使用pyplot和OO的方式，显示导入numpy
+'''
+
+
 import matplotlib.pyplot as plt
 # plt.plot([1,2,3,4],[-4,-3,-2,-1]) # 第一 个[]为x轴，第二个[]为y轴
 plt.plot([1,2,3,4],[-1,-2,-3,-4])
---
+++ pyplot/colorAndStyle.py
@@ -0,0 +1,101 @@
+'''
+八种内建默认颜色缩写：
+    b: blue
+    g: green
+    r: red
+    c: cyan
+    m: magenta
+    y: yellow
+    k: black
+    w: white
+其他颜色表示方法
+    灰色阴影
+    html 十六进制
+    RGB元祖
+'''
+
+
+
+'''
+23种点形状。注意，不同点形状默认使用不同颜色
+    "." : point        "v" : triangle_down
+    "," : pixel        "^" : triangle_up
+    "0" : circle       "<" : triangle_left
+    "1" : tri_down     ">" : triangle_right
+    "2" : tri_up       "s" : square
+    "3" : tri_left     "p" : pentagon
+    "4" : tri_right    "*" : star
+    "8" : octagon      "h" : hexagon1
+    "+" : plus         "H" : hexagon2
+    "x" : x            "D" : diamond
+    "d" : thin_diamond
+'''
+
+
+
+'''
+四种线型
+    -    实线
+    --   虚线
+    -.   点划线
+    :    点线
+
+'''
+
+
+
+'''
+样式字符串(可以将颜色、点型、线型写成一个字符串)，如：
+    cx--
+    mo:
+    kp-
+
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+y = np.arange(1,5)
+
+
+# 颜色举例
+# plt.plot(y,color='g') # 内置颜色
+
+# plt.plot(y+1,color='0.5') #灰度
+
+# plt.plot(y+2,color='#CDCDB4') # 十六进制
+
+# plt.plot(y+3,color=(0.1,0.2,0.3)) # RGB元祖
+
+
+
+
+# 点形状(写marker会将点连成线，不写则不连线)
+# plt.plot(y,marker='o')
+
+# plt.plot(y+1,marker='^')
+
+# plt.plot(y+2,'D')
+
+# plt.plot(y+3,'p')
+
+# 线型
+# plt.plot(y,'--')
+
+# plt.plot(y+1,'-.')
+
+# plt.plot(y+2,'-')
+
+# plt.plot(y+3,':')
+
+#样式字符串
+plt.plot(y,'cx--')
+
+plt.plot(y+1,'kp-.')
+
+plt.plot(y+2,'mo-')
+
+plt.plot(y+3,'y^:')
+
+
+plt.show()
